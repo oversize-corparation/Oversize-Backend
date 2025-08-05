2f288a703eb1da03d341507ed6ffa112be1a9403
shokhruzbek <ashokhruzbek@gmail.com>
2025-08-05 23:17:57 +0500

    Add CRUD operations for addresses with Swagger documentation

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -3,42 +3,46 @@ import swaggerJsDoc from "swagger-jsdoc";
 import { Express } from "express";
 
 const options: swaggerJsDoc.Options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Oversize API hujjatlari",
-            version: "1.0.0",
-            description: "Bu yerda barcha Auth, Email yuborish, va boshqa xizmatlar bo‘yicha API hujjatlari keltirilgan."
-        },
-        tags: [
-            {
-            name: 'Auth',
-            },
-            {
-            name: 'Users',
-            },
-        ],
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT', // ixtiyoriy, Swagger UI uchun ko‘rinish beradi
-                }
-            }
-        },
-        security: [
-            {
-                bearerAuth: [] // Har bir endpointga token kerakligini bildiradi
-            }
-        ]
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Oversize API hujjatlari",
+      version: "1.0.0",
+      description:
+        "Bu yerda barcha Auth, Email yuborish, va boshqa xizmatlar bo‘yicha API hujjatlari keltirilgan.",
     },
+    tags: [
+      {
+        name: "Auth",
+      },
+      {
+        name: "Users",
+      },
+      {
+        name: "Addresses"
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT", // ixtiyoriy, Swagger UI uchun ko‘rinish beradi
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [], // Har bir endpointga token kerakligini bildiradi
+      },
+    ],
+  },
 
-    apis: ['./src/routes/*.ts'], // Annotatsiya yozilgan fayllar yo'li
-}
+  apis: ["./src/routes/*.ts"], // Annotatsiya yozilgan fayllar yo'li
+};
 
 const swaggerSpec = swaggerJsDoc(options);
 
-export function swaggerSetup (app: Express){
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+export function swaggerSetup(app: Express) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
